2d31b3b711e0237d9900bf9b2a2e62416baab8ab
David de la Calle Cerezo <david.delacalle@kairosds.com>
2018-12-09 00:03:03 +0100

    Add loading overlay

---
+++ src/components/LoadingOverlay.vue
@@ -0,0 +1,62 @@
+<template>
+  <div id="loading-overlay" v-if="loading">
+    <p>Loading<span class="dot">.</span><span class="dot">.</span><span class="dot">.</span></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoadingOverlay',
+  props: {
+    loading: Boolean
+  }
+}
+</script>
+
+<style scoped>
+  #loading-overlay {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 1000;
+  }
+
+  #loading-overlay p {
+    color: #fff;
+    font-size: 2em;
+    text-align: center;
+    position: relative;
+    top: 50%;
+  }
+
+  @keyframes blink {
+    0% {
+      opacity: .2;
+    }
+    20% {
+      opacity: 1;
+    }
+    100% {
+      opacity: .2;
+    }
+  }
+
+  #loading-overlay .dot {
+    font-size: 2em;
+    animation-name: blink;
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+    animation-fill-mode: both;
+  }
+
+  #loading-overlay .dot:nth-child(2) {
+      animation-delay: .4s;
+  }
+
+  #loading-overlay .dot:nth-child(3) {
+      animation-delay: .8s;
+  }
+</style>
--- src/components/PortfolioOverview.vue
+++ src/components/PortfolioOverview.vue
@@ -1,21 +1,27 @@
 <template>
-  <div class="portfolio-overview">
-    <UseHeader
-      :user="user"
-    ></UseHeader>
-    <PortfolioSummary
-      :portfolio="portfolio"
-    ></PortfolioSummary>
-    <TransactionRow
-      v-for="(transaction, index)  in transactions"
-      :key="index"
-      :transaction="transaction"
-    ></TransactionRow>
-  </div>
+  <section class="section portfolio-overview">
+    <div class="container">
+      <LoadingOverlay :loading="loading"></LoadingOverlay>
+      <div v-if="!loading">
+        <UseHeader
+          :user="user"
+        ></UseHeader>
+        <PortfolioSummary
+          :portfolio="portfolio"
+        ></PortfolioSummary>
+        <TransactionRow
+          v-for="(transaction, index)  in transactions"
+          :key="index"
+          :transaction="transaction"
+        ></TransactionRow>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 
+import LoadingOverlay from '@/components/LoadingOverlay.vue'
 import PortfolioSummary from '@/components/PortfolioSummary.vue'
 import TransactionRow from '@/components/TransactionRow.vue'
 import UseHeader from '@/components/UserHeader.vue'
@@ -25,6 +31,7 @@ const DATA_SOURCE = '/data/data.json'
 export default {
   name: 'PortfolioOverview',
   components: {
+    LoadingOverlay,
     PortfolioSummary,
     TransactionRow,
     UseHeader
@@ -33,6 +40,7 @@ export default {
   },
   data: function () {
     return {
+      loading: Boolean,
       portfolio: Object,
       transactions: Object,
       user: Object
@@ -40,6 +48,7 @@ export default {
   },
   methods: {
     getTransactions: function getTransactions () {
+      this.loading = true
       fetch(DATA_SOURCE)
         .then(response => response.json())
         .then(json => {
@@ -48,9 +57,13 @@ export default {
           this.transactions = json.transfers.results
         })
         .catch(error => console.error(error))
+        .finally(() => {
+          this.loading = false
+        })
     }
   },
   created: function () {
+    this.loading = true
     this.getTransactions()
   }
 }
