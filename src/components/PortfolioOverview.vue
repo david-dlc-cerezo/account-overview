543a1a9da35954ab81989d1c37da95a4f149786a
David de la Calle Cerezo <david.delacalle@kairosds.com>
2018-12-09 00:44:42 +0100

    Init user postion

--- src/components/PortfolioOverview.vue
+++ src/components/PortfolioOverview.vue
@@ -3,12 +3,9 @@
     <div class="container">
       <LoadingOverlay :loading="loading"></LoadingOverlay>
       <div v-if="!loading">
-        <UseHeader
-          :user="user"
-        ></UseHeader>
-        <PortfolioSummary
-          :portfolio="portfolio"
-        ></PortfolioSummary>
+        <UseHeader :user="user"></UseHeader>
+        <UserPosition :position="userPosition" :currency="portfolio.currency"></UserPosition>
+        <PortfolioSummary :portfolio="portfolio"></PortfolioSummary>
         <TransactionRow
           v-for="(transaction, index)  in transactions"
           :key="index"
@@ -25,6 +22,7 @@ import LoadingOverlay from '@/components/LoadingOverlay.vue'
 import PortfolioSummary from '@/components/PortfolioSummary.vue'
 import TransactionRow from '@/components/TransactionRow.vue'
 import UseHeader from '@/components/UserHeader.vue'
+import UserPosition from '@/components/UserPosition.vue'
 
 const DATA_SOURCE = '/data/data.json'
 
@@ -34,7 +32,8 @@ export default {
     LoadingOverlay,
     PortfolioSummary,
     TransactionRow,
-    UseHeader
+    UseHeader,
+    UserPosition
   },
   props: {
   },
@@ -55,6 +54,18 @@ export default {
           this.portfolio = json.portfolios.results[0]
           this.user = json.user
           this.transactions = json.transfers.results
+
+          // Agregate all portfolios
+          this.userPosition = {
+            balance: 0,
+            invested: 0,
+            earnings: 0
+          }
+          json.portfolios.results.reduce((acc, { latest_allocation }) => {
+            acc.balance += latest_allocation.balance
+            acc.invested += latest_allocation.invested
+            acc.earnings += latest_allocation.earnings
+          }, this.userPosition)
         })
         .catch(error => console.error(error))
         .finally(() => {
---
+++ src/components/UserPosition.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="user-position container">
+    <div class="tile is-ancestor">
+      <div class="tile is-horizontal">
+        <article class="tile is-child">
+          <p class="title">Balance</p>
+          <p class="subtitle">
+            {{position.balance | currency(currency.code)}}
+          </p>
+        </article>
+        <article class="tile is-child">
+          <p class="title">Invested</p>
+          <p class="subtitle">
+            {{position.invested | currency(currency.code)}}
+          </p>
+        </article>
+        <article class="tile is-child">
+          <p class="title">Earnings</p>
+          <p class="subtitle">
+            {{position.earnings | currency(currency.code)}}
+          </p>
+        </article>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import currency from '@/filters/currency.js'
+
+export default {
+  name: 'UserPosition',
+  props: {
+    position: Object,
+    currency: Object
+  },
+  filters: {
+    currency
+  }
+}
+</script>
+
+<style scoped>
+.tile .title {
+  text-transform: uppercase;
+  font-size: 1em;
+}
+</style>
