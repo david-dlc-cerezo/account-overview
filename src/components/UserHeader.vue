cd13cfac1a0762ad90420741ba8ba93b1543051b
David de la Calle Cerezo <david.delacalle@kairosds.com>
2018-12-10 01:36:15 +0100

    Complete header layout design

--- src/components/UserHeader.vue
+++ src/components/UserHeader.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="user-header">
-    <div class="welcome">
-      Welcome back, <span class="welcome__name">{{user.last_name}}</span>!
+  <section class="user-header">
+    <div class="level">
+      <div class="level-left">
+        <div class="level-item is-half welcome">
+          Welcome back, <span class="welcome__name">{{user.last_name}}</span>!
+        </div>
+      </div>
+      <div class="level-right">
+        <div class="level-item is-half last-login">
+          <img src="/img/image-login.png" class="last-login-icon" alt="">
+          Last login: {{user.last_login | date}}, {{user.last_login | hour}}
+        </div>
+      </div>
     </div>
-    <div class="last-login">
-      Last login: {{user.last_login | date}}, {{user.last_login | hour}}
-    </div>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -26,7 +33,16 @@ export default {
 </script>
 
 <style scoped>
+  .welcome {
+    font-size: 2em;
+  }
   .welcome__name {
     color: #00a7d0;
+  }
+  .user-header {
+    margin-bottom: 1em;
+  }
+  .last-login-icon {
+    vertical-align: middle;
   }
 </style>
